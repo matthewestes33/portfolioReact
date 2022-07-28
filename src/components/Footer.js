8d45739c52f844230f668a8c05cc3a1b7ea4889e
Matt Estes <me6384@gmail.com>
2022-07-28 14:02:24 -0700

    Footer component established

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Footer() {   
+    return (
+    <div class="footer" id="footer">
+        <a href="https://github.com/matthewestes33"><img src="https://img.icons8.com/color/48/000000/github-2.png" alt="Github" class="icon"/></a>
+        <a href="https://linkedin.com/in/matthewestes33//"><img src="https://img.icons8.com/color/48/000000/linkedin.png" alt="LinkedIn" class="icon"/></a>
+        <p>
+        &copy; josephDeWoody  |  <a href="placeholder.pdf" class="link">download resume</a>  |  made with <img id="react-icon" src="https://img.icons8.com/color/48/000000/react-native.png" alt="React"/>
+        </p>
+
+    </div>
+    )
+}
+
+export default Footer;
