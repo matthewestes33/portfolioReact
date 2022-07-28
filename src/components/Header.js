6f5d10869e4417fa3a73195ddd6bfded035680f7
Matt Estes <me6384@gmail.com>
2022-07-28 13:58:25 -0700

    Header component established

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Route, HashRouter } from "react-router-dom";
+import About from '../../components/About';
+import Contact from '../../components/Contact';
+import Portfolio from '../../components/Portfolio';
+import Resume from '../../components/Resume';
+
+class Header extends Component {
+    render() {
+        return (
+            <HashRouter>
+
+                <div className="content">
+                    <Route path="/about" component={About} />
+                    <Route path="/contact" component={Contact} />
+                    <Route path="/portfolio" component={Portfolio} />
+                    <Route path="/resume" component={Resume} />
+                </div>
+            </HashRouter>
+        )
+    }
+}
+
+export default Header;
