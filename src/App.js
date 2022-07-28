ca1b6f0651bab38c73044576e239020eb0840330
Matt Estes <me6384@gmail.com>
2022-07-28 13:45:59 -0700

    updated file structure and main App file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import BucketList from './components/BucketList';
 
+// Run app
 function App() {
+
   return (
-    <div className="bucket-app">
-      <BucketList />
+    <div className="App">
+      <Header/>
+      <Footer/>
     </div>
   );
 }
