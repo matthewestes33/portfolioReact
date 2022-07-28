9d1119714e1e6fe7fd1e89ac6b73423a65781da5
Matt Estes <me6384@gmail.com>
2022-07-28 15:10:24 -0700

    Resume component established

---
+++ src/components/Resume.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+function Resume() {
+
+    return (
+        <section class="mb-5">
+            <h1 className="resume">Resume</h1>
+            <hr></hr>
+            <div class="row justify-content-center" id="resume">
+                <div class="mt-5 pl-5 pr-5">
+                    Full stack web developer in training with extensive experience in education, training, and development.
+                    Progressing to a certificate in full stack development (JavaScript MERN Stack) from the University of Arizona.
+                    10+ years experience as an adult educator, with recent expertise in translating in-person instruction to virtual and on-demand audiences.
+                    Similar experience in curriculum development for diverse audiences and stakeholders.
+                    Formal education includes a law degree, with significant work experience related to legal writing and research.
+                    Currently seeking to bring my web development expertise and creativity up to par with my existing communication, writing, and project management abilities.
+                    Positioned to provide unique perspectives on creating and implementing user stories into future projects by leveraging past experiences working in government, higher education, and boutique firms.
+                    Seeking to leverage my skillset with a like-minded team to build better experiences on the web.
+
+                    <p class="mt-5">
+                        <a href="https://https://www.linkedin.com/in/matthewestes33/"><img src="https://img.icons8.com/color/48/000000/linkedin-2.png" alt="LinkedIn" /></a>
+                    </p>
+                    <p>
+                        Download my full <a href="placeholder.pdf" class="link">resume</a>
+                        <br></br>
+                    </p>
+                </div>
+            </div>
+
+        </section>
+    );
+}
+
+export default Resume;
