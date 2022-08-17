20e6b1e33b639203f02e37236aaeff88b757375a
Matt Estes <me6384@gmail.com>
2022-08-17 12:33:08 -0700

    add email to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,11 +3,12 @@ import React from 'react';
 function Footer() {   
     return (
     <div class="footer" id="footer">
+        <a href="mailto:matthewestes@outlook.com"><img src="https://img.icons8.com/color/48/000000/send-mass-email.png" alt="Email" class="icon"/></a>
         <a href="https://github.com/matthewestes33"><img src="https://img.icons8.com/color/48/000000/github-2.png" alt="Github" class="icon"/></a>
         <a href="https://linkedin.com/in/matthewestes33/"><img src="https://img.icons8.com/color/48/000000/linkedin.png" alt="LinkedIn" class="icon"/></a>
         <a href="https://stackoverflow.com/users/19675990/me33"><img src="https://img.icons8.com/color/48/000000/stack-overflow.png" alt="Stack Overflow" class="icon"/></a>
         <p>
-        &copy; <a href = "mailto: matthewestes@outlook.com">Matthew Estes</a> |  Made With <img id="react-icon" src="https://img.icons8.com/color/48/000000/react-native.png" alt="React"/>
+        &copy; Matthew Estes |  Made With <img id="react-icon" src="https://img.icons8.com/color/48/000000/react-native.png" alt="React"/>
         </p>
 
     </div>
